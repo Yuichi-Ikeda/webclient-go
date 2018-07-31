7c127cd3d6a29023b6d2fd6f0bacf3c099ead0f1
Yuichi-Ikeda <yuichii@outlook.com>
2018-07-31 17:38:25 +0900

    1.1

--- webclient.go
+++ webclient.go
@@ -24,31 +24,31 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Your url path is %s. \n", r.URL.Path)
 
-	// call gRPC service
+    // call gRPC service
     msg = callgrpc()
     fmt.Fprintf(w, "Greeting from gRPC service : %s", msg)
 }
 
 func callgrpc() string {
-        conn, err := grpc.Dial(address, grpc.WithInsecure())
-        if err != nil {
-                log.Fatalf("did not connect: %v", err)
-        }
-        defer conn.Close()
-        c := pb.NewGreeterClient(conn)
+    conn, err := grpc.Dial(address, grpc.WithInsecure())
+    if err != nil {
+            log.Fatalf("did not connect: %v", err)
+    }
+    defer conn.Close()
+    c := pb.NewGreeterClient(conn)
 
-        // Contact the server and print out its response.
-        name := defaultName
-        if len(os.Args) > 1 {
-                name = os.Args[1]
-        }
-        ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-        defer cancel()
-        r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-        if err != nil {
-                log.Fatalf("could not greet: %v", err)
-        }
-        log.Printf("Greeting: %s", r.Message)
+    // Contact the server and print out its response.
+    name := defaultName
+    if len(os.Args) > 1 {
+        name = os.Args[1]
+    }
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+    if err != nil {
+        log.Fatalf("could not greet: %v", err)
+    }
+    log.Printf("Greeting: %s", r.Message)
 
-        return r.Message
+    return r.Message
 }
